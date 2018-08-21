76382906f732d82f1ce6283465d554c8617bfb03
Madeleine Marcano <madeleine1302@gmail.com>
2018-08-21 11:29:01 -0300

    cambios

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,18 @@
-const button = document.getElementById('btnList')
+//const button = document.getElementById('btnList')
 const contList = document.getElementById('listas')
 contList.style.display = 'none';
+cards.style.display = 'none';
 
-button.addEventListener('click', () => {
-    tableros.style.display = 'none';
-    contList.style.display = 'block';
-    addList();
-})
+// button.addEventListener('click', () => {
+//         tableros.style.display = 'none';
+//         contList.style.display = 'block';
+//         addList();
+//     })
+// function hideTablero() {
+//     tableros.style.display = 'none';
+//     contList.style.display = 'block';
+//     addList();
+// }
 
 function addList() {
 
@@ -15,16 +21,42 @@ function addList() {
     const inputList = document.createElement('input')
     inputList.setAttribute('id', 'inputList')
     inputList.placeholder = 'Introduzca el titulo de la lista'
+
     const boxBtnList = document.getElementById('boxBtnAddList')
-    const btnList = document.createElement('button')
-    btnList.setAttribute('class', 'btn btn-success')
-    btnList.innerText = 'Añardir Tarjeta'
-    btnList.setAttribute('id', 'btnAddList')
+    const btnListA = document.createElement('button')
+    btnListA.setAttribute('class', 'btn btn-success')
+    btnListA.innerText = 'Guardar'
+    btnListA.setAttribute('id', 'btnAddList')
+
 
     contList.appendChild(cont)
     cont.appendChild(boxInputList)
     boxInputList.appendChild(inputList)
+
     cont.appendChild(boxBtnList)
-    boxBtnList.appendChild(btnList)
+    boxBtnList.appendChild(btnListA)
+
+    btnListA.setAttribute('onclick', 'addListName()')
+}
+
+function addListName() {
+    let nameList = inputList.value;
+    console.log(nameList);
+    titleList.innerHTML = nameList;
+
+    boxBtnList.appendChild(btnListA)
+    cards.style.display = 'block'
 
 }
+
+function addTitle() {
+    let titleList = inputList.value
+    console.log(titleList)
+    const contTitle = document.getElementById('titleList')
+    const boxTitleList = document.createElement('input')
+    boxTitleList.textContent = titleList
+
+    contTitle.appendChild(boxTitleList)
+}
+
+btnListA.addEventListener('click', addTitle())
---
+++ src/js/main.js
@@ -0,0 +1,78 @@
+let inputList = '';
+let btnListA = '';
+const contList = document.getElementById('listas')
+const tab = document.getElementById('tableros')
+const btnAddList = document.createElement('a')
+formList.style.display = 'none';
+cards.style.display = 'none'
+
+
+btnAddList.setAttribute('class', 'btn list')
+btnAddList.setAttribute('id', 'btnList')
+btnAddList.textContent = '+ Añadir una lista'
+
+tab.appendChild(btnAddList)
+
+function hideTablero() {
+    tab.style.display = 'none'
+    formList.style.display = 'block'
+
+}
+
+tab.addEventListener('click', () => {
+    hideTablero()
+
+    function addList() {
+
+        const cont = document.getElementById('formList')
+        const boxInputList = document.getElementById('boxInputList')
+        inputList = document.createElement('input')
+        inputList.setAttribute('id', 'inputList')
+        inputList.placeholder = 'Introduzca el titulo de la lista'
+
+        const boxBtnList = document.getElementById('boxBtnAddList')
+        btnListA = document.createElement('button')
+        btnListA.setAttribute('class', 'btn btn-success')
+        btnListA.innerText = 'Guardar'
+        btnListA.setAttribute('id', 'btnAddList')
+        btnListA.onclick = function() { titleList() }
+
+        contList.appendChild(cont)
+        cont.appendChild(boxInputList)
+        boxInputList.appendChild(inputList)
+
+        cont.appendChild(boxBtnList)
+        boxBtnList.appendChild(btnListA)
+    }
+    addList()
+
+})
+
+function titleList() {
+    cards.style.display = 'block'
+    let nameTitleList = inputList.value
+
+
+    const contTitle = document.getElementById('cards')
+    const boxTitle = document.getElementById('titleList')
+    const boxTitleList = document.createElement('strong')
+    boxTitleList.textContent = nameTitleList
+    contTitle.appendChild(boxTitle)
+    boxTitle.appendChild(boxTitleList)
+
+    const boxCard = document.getElementById('addCard')
+    const addTask = document.createElement('a')
+    addTask.href = '#';
+    const addTaskC = document.createElement('span')
+    addTaskC.textContent = '+ Añadir una Tarjeta'
+
+
+    contTitle.appendChild(boxCard)
+    boxCard.appendChild(addTask)
+    addTask.appendChild(addTaskC)
+
+}
+
+function addCards() {
+    const cards = document.getElementById
+}
